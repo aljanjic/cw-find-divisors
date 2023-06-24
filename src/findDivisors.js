9e527ea840080d8795b4f99c90576c90bd4cba33
Aljosa Janjic <janjic_aljosa@hotmail.com>
2023-06-24 19:49:41 +0200

    3rd test passed

--- src/findDivisors.js
+++ src/findDivisors.js
@@ -1,5 +1,7 @@
 function findDivisors(int) {
 
+  if(typeof int != 'number') return 'invalid';
+
   if(int === 6)return [2,3]
 
   return [3];
--- src/findDivisors.test.js
+++ src/findDivisors.test.js
@@ -6,6 +6,10 @@ describe('findDivisors', () => {
   });
 
   it('should return [2,3] when given 6', () => {
-    expect(findDivisors(3)).toStrictEqual([3]);
+    expect(findDivisors(2,3)).toStrictEqual([3]);
+  });
+
+  it('should return "invalid" when given string', () => {
+    expect(findDivisors('hello')).toStrictEqual('invalid');
   });
 });
